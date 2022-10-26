996b863fc479d339c37a3507d257d8a90fb6b2fe
roopesh-player <82817169+roopesh-player@users.noreply.github.com>
2022-10-26 12:31:33 +0530

    size change added in sieve

--- src/Sub-Apps/Sieve.jsx
+++ src/Sub-Apps/Sieve.jsx
@@ -3,21 +3,27 @@ import React,{useState,useEffect }  from 'react'
 const Sieve = () => {
 
  const [array, setarray] = useState([])
+ const [arraySize, setarraySize] = useState('50')
 
   useEffect(() => {
     // funt();
+    function FillArray(){
+      let n=parseInt(arraySize);
+      let newarray=[]
+      for(let i=0;i<n;i++){
+        newarray.push(i+1);
+      }
+      setarray(newarray);
+    }
     FillArray();
-  },[]);
-  function changesize(){
+  },[arraySize]);
+  function changesize(ev){
+    let preparse=ev.target.value;
+    console.log(preparse);
+    if(preparse!==arraySize)setarraySize(preparse);
     return;
   }
-  function FillArray(){
-    let newarray=[]
-    for(let i=0;i<100;i++){
-      newarray.push(i+1);
-    }
-    setarray(newarray);
-  }
+
   function AddDelay(milisec) { 
     return new Promise(resolve => { 
         setTimeout(() => { resolve('') }, milisec); 
@@ -31,20 +37,19 @@ const Sieve = () => {
     //handling case for 1
     // console.log("reached");
     // console.log(numberArray[1]);
-    numberArray[0].style.backgroundColor='orange';
+    numberArray[0].style.backgroundColor='yellow';
     await AddDelay(30);
     numberArray[0].style.backgroundColor='red';
     let lastprime=2;
     for (let p = 2; p * p <= n; p++){
         if (prime[p] === true){
-          numberArray[p-1].style.backgroundColor='orange';
+          numberArray[p-1].style.backgroundColor='yellow';
           lastprime=p;
             // Update all multiples of p
           for (let i = p * p; i <= n; i += p){
                 prime[i] = false;
-                numberArray[i-1].style.backgroundColor='yellow';
-                await AddDelay(50);
                 numberArray[i-1].style.backgroundColor='red';
+                await AddDelay(100);
           }
           await AddDelay(30);
           numberArray[p-1].style.backgroundColor='lime';
@@ -59,13 +64,21 @@ const Sieve = () => {
       }
     }
   }
-
+  function reset(){
+    const numberArray=document.getElementsByClassName("border border-primary m-3 rounded d-flex align-items-center justify-content-center");
+    let n=numberArray.length;
+    for(let i=0;i<n;i++)numberArray[i].style.backgroundColor='aqua';
+  }
 
   return (
     <div style={{height:"auto",background:"black"}}>
+      {/* {FillArray()} */}
       <nav className='d-flex justify-content-center'>
       <button className='btn btn-outline-success rounded-0' onClick={() => sievealgo()}>Start</button>
+      <button className='btn btn-outline-info rounded-0' onClick={() => reset()}>Reset</button>
+      {/* <input type='text' value={arraySize} onChange={e => changesize(e)} /> */}  
     </nav>
+    <span className='text-info d-flex justify-content-center'>Size <input type="range" id="myRange" min="25" max="150" value={arraySize} onChange={e => changesize(e)}/></span>
     <div className='container' >
     <div className="row p-5">
     {array.map((value,id)=>{
